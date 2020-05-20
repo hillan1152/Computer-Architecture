616f1b5999c388bf7f1028a8c487fbc5adb3f1c6
Anthony Hill <hillan1152@gmail.com>
2020-05-20 15:40:16 -0400

    added operand change for self.pc

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,6 +7,8 @@ LDI = 0b10000010 # LDI R0,8
 PRN = 0b01000111 # PRN R0
 HLT = 0b00000001 # HLT
 MUL = 0b10100010 # MUL
+PUSH = 0b01000101
+POP = 0b01000110
 
 class CPU:
     """Main CPU class."""
@@ -20,36 +22,71 @@ class CPU:
         self.branchtable[LDI] = self.handle_LDI
         self.branchtable[PRN] = self.handle_PRN
         self.branchtable[MUL] = self.handle_MUL
+        self.branchtable[PUSH] = self.handle_push
+        self.branchtable[POP] = self.handle_pop
         # INTERNAL REGISTERS
         self.pc = 0 # keeps address of currently executing instruction
-        self.halted = False
-
+        self.halted = False # lets 
+        self.sp = 7 # stack pointer, only for the stack
+        self.reg[self.sp] = 0xf4 # Start at f4 aka 244
         # self.FL  = [0] * 0b00001000 # 8 bits
 
         # Registers External
         # self.IM = self.reg[6] # interrupt mask
         # self.IS = self.reg[7] # interrupt status
-        # self.SP = self.reg[8] # stack pointer
 
     def handle_HLT(self):
         self.halted = True
     
     def handle_LDI(self):
-        operand_a = self.ram_read(self.pc + 1)
-        operand_b = self.ram_read(self.pc + 2)
+        operand_a = self.ram_read(self.pc + 1) # 00000000
+        operand_b = self.ram_read(self.pc + 2) # 00000001
         self.reg[operand_a] = operand_b
-        self.pc += 3
+        # self.pc += 3
 
     def handle_PRN(self):
         operand_a = self.ram_read(self.pc + 1)
         print(self.reg[operand_a])
-        self.pc += 2
+        # self.pc += 2
 
     def handle_MUL(self):
         operand_a = self.ram_read(self.pc + 1)
         operand_b = self.ram_read(self.pc + 2)
         self.alu("MUL", operand_a, operand_b)
-        self.pc += 3
+        # self.pc += 3
+
+    def handle_push(self):
+        # operand_a is the next available spot
+        operand_a = self.ram_read(self.pc + 1)
+        # Decrement Value of the Stack Pointer
+            # REG[7] == 0, After == -1 
+        self.reg[self.sp] -= 1
+        # stores value of reg @ sp into the ram, stores the next spot AT BACK OF RAM 
+            # reg [0, 1, 2, 3, 4, 5, 6, 7]
+            # ram [0, 0, 0, 0, 0, 0...]
+            # PUSH --- num after ---> 28
+            # ram[-1] == value after push is called
+            # ram [0, 0, 0, 0, 0, 0... 28] --> stack starts at 28
+        """
+        Process
+            # print("TOP OF STACK ADDRESS", self.reg[self.sp])
+            # print("Ram Current Value", self.ram[self.reg[self.sp]])
+            # print("Replacement Value", operand_a)
+        """
+        self.ram[self.reg[self.sp]] = operand_a
+        # moves address 2 spots
+        # print("Ram Total", self.ram)
+        # self.pc += 2
+        
+
+    def handle_pop(self):
+        # Retrieve value from RAM @ SP
+        print("POP", self.ram[self.reg[self.sp]])
+        # Store Value in Reg
+        # increment SP
+        # self.pc += 2
+        
+
 
     def load(self):
         """Load a program into memory."""
@@ -85,9 +122,10 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        if op == "MUL":
+        elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "SUB":
+            self.reg[reg_a] -= self.reg[reg_b] 
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -125,24 +163,19 @@ class CPU:
         # store results in IR
 
         # needs: PC, IR, ram_read
-        # LDI = 0b10000010 # LDI R0,8
-        # PRN = 0b01000111 # PRN R0
-        # HLT = 0b00000001 # HLT
-        # MUL = 0b10100010 # MUL
-        
         while not self.halted:
             IR = self.ram_read(self.pc)
 
             if IR in self.branchtable:
                 self.branchtable[IR]()
+                self.pc += (IR >> 6) + 1
             else:
                 print(f"unknown instruction {IR} at address {self.pc}")
                 exit(1)
 
-        
-        
-        
-        
+        """
+        IF/ELSE STATEMENTS BELOW
+        """
         # while not self.halted:
         #     IR = self.ram_read(self.pc)
         #     # memory 1 spot after current
